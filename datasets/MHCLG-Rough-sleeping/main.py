7f4507773f416f721a29a1c531301d68345855d9
LPerryman <leigh.perryman@gsscogs.uk>
2020-04-24 12:12:26 +0100

    messing with json files

--- datasets/MHCLG-Rough-sleeping/main.py
+++ datasets/MHCLG-Rough-sleeping/main.py
@@ -35,41 +35,64 @@ except Exception as e:
     print(e.message, e.args)
 
 
-def extract_table(tab, ref1, ref2, ref3, ref4, title1, title2, title3):
+def extract_table(tab, ref1, ref2, ref3, ref4, ref5, ref6, ref7, title1, title2, title3, title4, title5, title6):
     try:
         col1 = tab.excel_ref(ref1).fill(DOWN).is_not_blank()
         col2 = tab.excel_ref(ref2).fill(DOWN).is_not_blank()
-        col3 = tab.excel_ref(ref3).expand(RIGHT).is_not_blank() 
-        col4 = tab.excel_ref(ref4).is_not_blank()
+        col3 = tab.excel_ref(ref3).fill(DOWN).is_not_blank()
+        col4 = tab.excel_ref(ref4).expand(RIGHT).is_not_blank() 
+        col5 = tab.excel_ref(ref5).expand(RIGHT).is_not_blank() 
+        col6 = tab.excel_ref(ref6).expand(RIGHT).is_not_blank() 
+        col7 = tab.excel_ref(ref7).is_not_blank()
 
         Dimensions = [
             HDim(col1,title1, DIRECTLY, LEFT),
             HDim(col2,title2, DIRECTLY, LEFT),
-            HDim(col3,title3, DIRECTLY, ABOVE)
+            HDim(col3,title3, DIRECTLY, LEFT),
+            HDim(col4,title4, DIRECTLY, ABOVE),
+            HDim(col5,title5, DIRECTLY, ABOVE),
+            HDim(col6,title6, DIRECTLY, ABOVE)
             ]
 
-        c1 = ConversionSegment(col4, Dimensions, processTIMEUNIT=True)
+        c1 = ConversionSegment(col7, Dimensions, processTIMEUNIT=True)
         tbl = c1.topandas()
         
+        # Get rid of any columns that have the word Spare in their name
+        for col in tbl.columns:
+            if 'Spare' in col:
+                tbl = tbl.drop(col, 1)
+                
         return tbl
     except Exception as e:
         return "extract_sheet: " + str(e)   
 
 
-dataset_title = info['dataset1']['name'] 
-sheet_name = info['dataset1']['sheet1']['name']
-col_names = info['dataset1']['sheet1']['columnnames']
-coords = info['dataset1']['sheet1']['coords']
-print(dataset_title)
-print(sheet_name)
-print(col_names)
-print(coords)
+tbl_set = []
+no_of_datasets = int(info['noofdatasets'])
+for x in range(no_of_datasets):
+    x = x + 1
+    no_of_sheets = int(info['dataset' + str(x)]['noofsheets'])
+    for y in range(no_of_sheets):
+        try:
+            y = y + 1
+            dataset_title = info['dataset' + str(x)]['name'] 
+            sheet_name = info['dataset' + str(x)]['sheet' + str(y)]['name']
+            col_names = info['dataset' + str(x)]['sheet' + str(y)]['columnnames']
+            coords = info['dataset' + str(x)]['sheet' + str(y)]['coords']
+            print('Title: ' + dataset_title + ' - Sheet Name: ' + sheet_name)
+            print(col_names)
+            print(coords)
 
-tbl = extract_table([t for t in sheets if t.name == 'table 1'][0], 
-        coords[0], coords[1], coords[2], coords[3], 
-        col_names[0], col_names[1], col_names[2])
-tbl
+            tbl = extract_table([t for t in sheets if t.name == sheet_name][0], 
+                    coords[0], coords[1], coords[2], coords[3], coords[4], coords[5], coords[6],  
+                    col_names[0], col_names[1], col_names[2], col_names[3], col_names[4], col_names[5])
+            tbl_set.append(tbl)
+            del tbl
+        except Exception as e:
+            print('Error: ' + str(e))
 
-tbl['Period'].unique()
+tbl_set[2]
+
+tbl_set[3]
 
 

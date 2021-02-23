553e11dc312dc902a71066641ce26305fad5775f
canwaf <andrew.fergusson@me.com>
2021-02-23 12:30:19 +0000

    wg-homelessness-statistics Initial transform done on #8

--- datasets/WG-Homelessness-statistics/main.py
+++ datasets/WG-Homelessness-statistics/main.py
@@ -1,6 +1,8 @@
 # # WG Homelessness statistics 
 
 from gssutils import * 
+import pandas as pd
+import numpy as np
 import json 
 
 infoFileName = 'info.json'
@@ -15,6 +17,9 @@ df = distro.as_pandas()
 
 df.head()
 
+# Clearing all blank strings
+df = df.replace(r'^\s*$', np.nan, regex=True)
+
 # For everything which isn't the Data column, it's categorical so...
 for col in df.columns:
     if col != 'Data':
@@ -26,22 +31,37 @@ for c in df.columns:
 
 df.columns
 
-# Likely not required or dupliate
-drop_list = ['Area_Code', # Not required
-             'Area_ItemName_ENG', # Not required
-             'Area_SortOrder', # Not required
-             'Area_Hierarchy', # Not required
-             'Measure_SortOrder', # Not required
-             'Measure_Code', # Note required
-             'Measure_ItemNotes_ENG', # All blanks
-             'Year_Code', # Not required
-             'Year_SortOrder', # Not required
-             'Year_ItemNotes_ENG', # All blanks
-             'RowKey', # Not required
-             'PartitionKey'] # Not required
-df.drop(drop_list, inplace=True, axis=1)
+# Throw an error if item notes which we don't expect have any values
+# df['Household_ItemNotes_ENG'].count()
+for col in ['Household_ItemNotes_ENG', 'Period_ItemNotes_ENG']:
+    if df[col].count() != 0:
+        print(col, df[col].count())
+        raise Exception('New Datamakers not transformed')
 
-df.head()
+# Likely not required or dupliate
+drop_list = [
+    'Area_Code', # Not required
+    'Area_ItemName_ENG', # All blank
+    'Area_SortOrder', # Not required
+    'Area_Hierarchy', # Not required
+    'Area_ItemNotes_ENG', # Emergency bed availability in Swansea not applicable for this dataset
+    'Outcomes_Code', # Not required
+    'Outcomes_SortOrder', # Not required
+    'Outcomes_Hierarchy', # Not required
+    'Outcomes_ItemNotes_ENG', # Only one value which isn't necessary as it is evident from data availability
+    'Household_ItemNotes_ENG', 
+    'Household_Code', # Not required
+    'Household_SortOrder', # Not required
+    'Household_ItemNotes_ENG', # All blank (checked in cell above - must be included otherwise)
+    'Period_ItemName_ENG', # Not required
+    'Period_SortOrder', # Not required
+    'Period_ItemNotes_ENG', # All blank (checked in cell above - must be included otherwise)
+    'RowKey', # Not required
+    'PartitionKey' # Not required
+]
+
+
+df.drop(drop_list, inplace=True, axis=1)
 
 # Data is the value, and is a count of people so...
 df['Value'] = df['Data'].astype(int)
@@ -51,33 +71,38 @@ df.drop('Data', inplace=True, axis=1)
 df['Geography'] = df['Area_AltCode1'].apply(lambda x: "{}{}".format('http://statistics.data.gov.uk/id/statistical-geography/', x))
 df.drop('Area_AltCode1', inplace=True, axis=1)
 
-# Marker (For the geography though it applies to values as well)
-df['Marker'] = df['Area_ItemNotes_ENG'].apply(lambda x: " ".join(x.split()))
-df.drop('Area_ItemNotes_ENG', inplace=True, axis=1)
-
 # Measure
-df.rename({'Measure_ItemName_ENG': 'Measure', 'Year_ItemName_ENG': 'Period'}, inplace=True, axis=1)
+df.rename({'Measure_ItemName_ENG': 'Measure'}, inplace=True, axis=1)
 df.head()
 
-# From the metadata, which is incomplete so therefor not implemented
-exact_count = {
-    '2015-16': '/id/georgean-interval/2015-11-25T23:00:00/P4H',
-    '2016-17': '/id/georgean-interval/2016-11-03T22:00:00/P7H',
-    '2017-18': '/id/georgean-interval/2017-11-09T22:00:00/P7H',
-    '2018-19': '/id/georgean-interval/2018-11-08T22:00:00/P7H'
-}
-estimates = {
-    '2015-16': '/id/georgean-interval/2015-11-02/P2W',
-    '2016-17': '/id/georgean-interval/2016-11-10/P2W',
-    '2017-18': '/id/georgean-interval/2017-11-15/P2W',
-    '2018-19': '/id/georgean-interval/2018-11-16/P2W'  
-}
+# For periods
+df['Period_Code'].cat.categories
 
-# For the periods
-df['Period'] = df['Period'].apply(lambda x: '/id/government-year/{}-{}'.format(x[:4], '20'+x[-2:]))
+# ### The periods require a bit more work. From the odata...
+# ```
+# {
+#       "Data":-999999999.0,"Area_Code":"546","Area_ItemName_ENG":"Torfaen",
+#       "Area_SortOrder":"24","Area_Hierarchy":"803","Area_ItemNotes_ENG":"",
+#       "Area_AltCode1":"W06000020","Outcomes_Code":101.0,
+#       "Outcomes_ItemName_ENG":"Ineligible households","Outcomes_SortOrder":"29",
+#       "Outcomes_Hierarchy":160.0,"Outcomes_ItemNotes_ENG":"","Household_Code":"8",
+#       "Household_ItemName_ENG":"Single person household","Household_SortOrder":"7",
+#       "Household_ItemNotes_ENG":"","Period_Code":"201516Q3",
+#       "Period_ItemName_ENG":"2015-16 October-December","Period_SortOrder":"7704",
+#       "Period_ItemNotes_ENG":"","RowKey":"0000000000000172","PartitionKey":""
+# }
+# ```
+#
+# October-December is assigned a value of 201516Q3, which means these are government year and quarters not calendar years and quarters. If the value has a q in it it is a government quarter, otherwise a government year. Enter lambda functions with an anonymous dictionary
 
+df['Period_Code'].cat.rename_categories(lambda x: {True: f"/id/government-quarter/{x[:4]}-Q{x[-1:]}", False: f"/id/government-year/{x[:4]}-{x[-3:-1]}"}.get("Q" in x), inplace=True)
+df.rename({'Period_Code': 'Period'}, inplace=True)
 
-df
+df.head()
+
+df['Outcomes_ItemName_ENG'].cat.rename_categories(lambda x: utils.pathify(x), inplace=True)
+
+df.head()
 
 # Add dataframe is in the cube
 cubes.add_cube(scraper, df, scraper.title)

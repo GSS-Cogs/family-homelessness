07c74e35db99ddc6ac122137664104337a4af4b9
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-03-25 18:06:55 +0000

    Converted ODS to XLS but it is not the required distribution and hence required distrubution to be obtained before any further progress.

--- datasets/MHCLG-Statutory-homelessness-Detailed-local-authority-level/main.py
+++ datasets/MHCLG-Statutory-homelessness-Detailed-local-authority-level/main.py
@@ -2,14 +2,43 @@
 
 from gssutils import * 
 import json 
+import pandas as pd
+import numpy as np 
+from io import BytesIO
+import pyexcel
+import messytables
 
+#  get all the distributions
 info = json.load(open('info.json')) 
-landingPage = info['landingPage'] 
-landingPage 
+scraper = Scraper(seed = "info.json")
+scraper.distributions = [x for x in scraper.distributions if hasattr(x, "mediaType")] 
+scraper
+ 
 
-# + 
-#### Add transformation script here #### 
+original_tabs = scraper.distribution(latest = True, mediaType = ODS)
+original_tabs
 
-scraper = Scraper(landingPage) 
-scraper.select_dataset(latest=True) 
-scraper 
+
+with original_tabs.open() as ods_obj:
+    excel_obj = BytesIO()
+    book = pyexcel.get_book(file_type = 'ods', file_content = ods_obj, library = 'pyexcel-ods3')
+    old_tab_names = book.sheet_names()
+    
+    for old_tab in old_tab_names:
+        if len(old_tab) > 31:
+            new_tab_names = book.sheet_names()
+            find_index = new_tab_names.index(old_tab)
+            book.remove_sheet(book.sheet_names()[find_index])
+            
+    book.save_to_memory(file_type = 'xls', stream = excel_obj)
+    tableset = messytables.excel.XLSTableSet(fileobj = excel_obj)
+    tabs = list(xypath.loader.get_sheets(tableset, "*"))
+
+# +
+ 
+distribution = scraper.distribution(latest = True, mediaType=ODS)
+datasetTitle = distribution.title
+datasetTitle
+# -
+
+

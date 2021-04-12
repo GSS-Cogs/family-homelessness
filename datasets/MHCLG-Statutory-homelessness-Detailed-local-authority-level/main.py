3f7dccd54c08fe4963f20c55fc57751318b80fcc
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-04-12 11:42:28 +0100

    Transformation of Sheet "R2"

--- datasets/MHCLG-Statutory-homelessness-Detailed-local-authority-level/main.py
+++ datasets/MHCLG-Statutory-homelessness-Detailed-local-authority-level/main.py
@@ -353,7 +353,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A4R']: #only transforming tab A4P for now
+    if tab.name in ['A4R']: #only transforming tab A4R for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -405,7 +405,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A5P']: #only transforming tab A4P for now
+    if tab.name in ['A5P']: #only transforming tab A5P for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -456,7 +456,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A5R']: #only transforming tab A4P for now
+    if tab.name in ['A5R']: #only transforming tab A5R for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -514,7 +514,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A6_']: #only transforming tab A4P for now
+    if tab.name in ['A6_']: #only transforming tab A6_ for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -562,7 +562,7 @@ df
 # for tab in tabs:
 #     columns=['TO DO']
 #     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-#     if tab.name in ['A7']: #only transforming tab A4P for now
+#     if tab.name in ['A7']: #only transforming tab A7 for now
 #         print(tab.name)
         
 #         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -599,7 +599,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A8']: #only transforming tab A4P for now
+    if tab.name in ['A8']: #only transforming tab A8 for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -656,7 +656,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A10']: #only transforming tab A4P for now
+    if tab.name in ['A10']: #only transforming tab A10 for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -712,7 +712,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['A12']: #only transforming tab A4P for now
+    if tab.name in ['A12']: #only transforming tab A12 for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -769,7 +769,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['P1']: #only transforming tab A4P for now
+    if tab.name in ['P1']: #only transforming tab P1 for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -831,7 +831,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['P2']: #only transforming tab A4P for now
+    if tab.name in ['P2']: #only transforming tab P2 for now
         print(tab.name)
         
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -895,7 +895,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['P3']: #only transforming tab A4P for now
+    if tab.name in ['P3']: #only transforming tab P3 for now
         print(tab.name)
               
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -959,7 +959,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['P5']: #only transforming tab A4P for now
+    if tab.name in ['P5']: #only transforming tab P5 for now
         print(tab.name)
               
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -1026,7 +1026,7 @@ df
 for tab in tabs:
     columns=['TO DO']
     trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
-    if tab.name in ['R1']: #only transforming tab A4P for now
+    if tab.name in ['R1']: #only transforming tab R1 for now
         print(tab.name)
               
         remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
@@ -1087,6 +1087,79 @@ df.drop(['accomodation_secured_at_end_of_prevention_duty', 'gender'], axis=1, in
 df.rename(columns={'OBS' : 'Value', 'DATAMARKER' : 'Marker'}, inplace=True)
 df["Period"]= df["Period"].str.split(",", n = 1, expand = True)[1]
 df
+# +
+# Number of households whose relief duty ended by type of accommodation secured England
+
+for tab in tabs:
+    columns=['TO DO']
+    trace.start(datasetTitle, tab, columns, original_tabs.downloadURL)
+    if tab.name in ['R2']: #only transforming tab R2 for now
+        print(tab.name)
+              
+        remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
+        ons_geo = tab.excel_ref('A3').fill(DOWN).is_not_blank() - remove_notes # "-" suppressed in geography code to be processed in stage-2 transformation
+        period = tab.excel_ref('A1').is_not_blank() #period can be extracted from this cell 
+        sheet_name = tab.name
+#         savepreviewhtml(remove_notes, fname= tab.name + "PREVIEW.html")
+        
+        accomodation_secured_at_end_of_relief_duty = tab.excel_ref('D3').expand(RIGHT)
+        break_down = tab.excel_ref('D4').expand(RIGHT)
+        break_down_of_PRS_SRS = tab.excel_ref('D5').expand(RIGHT)
+        observations = tab.excel_ref('D6').expand(DOWN).expand(RIGHT).is_not_blank() - remove_notes
+#         savepreviewhtml(observations, fname= tab.name + "PREVIEW.html")
+        dimensions = [
+            HDim(ons_geo,'ONS Geography Code',DIRECTLY,LEFT),
+            HDim(period,'Period',CLOSEST,ABOVE),
+            HDim(accomodation_secured_at_end_of_relief_duty,'accomodation_secured_at_end_of_relief_duty',DIRECTLY, ABOVE),
+            HDim(break_down,'break_down',DIRECTLY, ABOVE),
+            HDim(break_down_of_PRS_SRS,'break_down_of_PRS_SRS',DIRECTLY,ABOVE),
+#             HDimConst("sheet_name", sheet_name) #Might be handy to have for post processing when other tabs are running also 
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname= tab.name + "PREVIEW.html")
+        trace.with_preview(tidy_sheet)
+        trace.store("combined_dataframe", tidy_sheet.topandas())
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+
+#sheet:A1
+df.drop(['temp_assessment_duty_type_1', 'temp_assessment_duty_type_2', 'temp_assessment_duty_type_3'], axis=1, inplace=True)
+#sheet:A2P
+df.drop(['reason_for_loss_of_home_1', 'end_of_tenancy_2', 'reason_for_end_of_tenancy_3', 'change_of_circumstances_4'], axis=1, inplace=True)
+#sheet:A2R_
+df.drop(['relief_duty_by_reason', 'end_of_AST', 'reason_for_end_of_AST', 'reason_for_rent_arrears'], axis =1, inplace=True)
+# sheet:A3
+df.drop(['total_no_of_households', 'reason_of_households_with_support_needs', 'total_households_and_no_of_people_with_support_needs'], axis=1, inplace=True)
+#sheet:A4P
+df.drop(['prevention_duty_owed_by_sector', 'prs_srs_homeless_on_departure_from_institution', 'status_of_occupation'],axis=1,inplace=True)
+#Sheet = A2R
+df.drop(['relief_duty_owed_by_sector', 'relief_prs_srs_homeless_on_departure_from_institution', 'relief_status_of_occupation'], axis=1, inplace=True)
+# Sheet = A5P
+df.drop(['prevention_duty_owed_by_household', 'single_parent_adult_male_female'], axis=1, inplace=True)
+# Sheet = A5R
+df.drop(['relief_duty_owed_by_household', 'relief_single_parent_adult_male_female'], axis=1, inplace=True)
+#Sheet = A6_
+df.drop(['age_of_main_applicants'], axis=1, inplace=True)
+#Sheet = A8
+df.drop(['ethnicgroup', 'breakdown_of_ethnicgroup'], axis=1, inplace=True)
+#Sheet = A10
+df.drop(['employment_status'],axis=1, inplace=True)
+#Sheet = A12
+df.drop(['sexual_identification'], axis=1, inplace=True)
+#Sheet = P1
+df.drop(['prevention_duty_ended', 'accomodation'], axis=1, inplace=True)
+#Sheet = P2
+df.drop(['prevention_duty_ended_accomodation_secured', 'prs_and_srs', 'tenancy_type'], axis=1, inplace=True)
+#Sheet = P3
+df.drop(['type_of_secured_accomodation'], axis=1, inplace=True)
+#Sheet = P5
+df.drop(['accomodation_secured_at_end_of_prevention_duty', 'gender'], axis=1, inplace=True)
+#Sheet = R1
+df.drop(['end_of_relief_duty'], axis=1, inplace=True)
+
+df.rename(columns={'OBS' : 'Value', 'DATAMARKER' : 'Marker'}, inplace=True)
+df["Period"]= df["Period"].str.split(",", n = 1, expand = True)[1]
+df
 # -
+
 
 

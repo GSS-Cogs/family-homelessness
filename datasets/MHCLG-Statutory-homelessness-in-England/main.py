55fcae7d67415ad0f5b9b909d31c160fab232ba9
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-04-19 09:08:20 +0100

    Transformaed first three tabs

--- datasets/MHCLG-Statutory-homelessness-in-England/main.py
+++ datasets/MHCLG-Statutory-homelessness-in-England/main.py
@@ -53,13 +53,115 @@ for tab in tabs:
     print(tab.name)
 
 # +
-#Now start the transformation
+#Number of households by initial assessment of homelessness circumstances and needs England
 
 for tab in tabs:
     columns=['Contents']
     trace.start(datasetTitle, tab, columns, distribution.downloadURL)
     if tab.name in ['A1']: #only transforming tab A1 for now
         print(tab.name)
+        
+        remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
+        quarter = tab.excel_ref('B6').expand(DOWN)-remove_notes
+        period = quarter.shift(LEFT).is_not_blank()-remove_notes 
+#         sheet_name = tab.name
+#         savepreviewhtml(period, fname= tab.name + "PREVIEW.html")
+    
+        initial_assessment = tab.excel_ref('D3').expand(RIGHT)
+        duty_owed = tab.excel_ref('D4').expand(RIGHT)
+        section_21 = tab.excel_ref('D5').expand(RIGHT)
+        observations = tab.excel_ref('D6').expand(RIGHT).expand(DOWN)-remove_notes
+#         savepreviewhtml(observations, fname= tab.name + "PREVIEW.html")
+        dimensions = [
+            HDim(quarter,'quarter',DIRECTLY,LEFT),
+            HDim(period,'period',CLOSEST,ABOVE),
+            HDim(initial_assessment,'initial_assessment',DIRECTLY, ABOVE),
+            HDim(duty_owed,'duty_owed',DIRECTLY, ABOVE),
+            HDim(section_21,'section_21',DIRECTLY, ABOVE),
+            #HDimConst("sheet_name", sheet_name) #Might be handy to have for post processing when other tabs are running also 
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname= tab.name + "PREVIEW.html")
+        trace.with_preview(tidy_sheet)
+        trace.store("combined_dataframe", tidy_sheet.topandas())
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+df
 # -
+
+#Number of households owed a prevention duty by reason for threat of loss, of last settled home England
+for tab in tabs:
+    columns=['Contents']
+    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
+    if tab.name in ['A2P']: #only transforming tab A1 for now
+        print(tab.name)
+        
+        remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
+        quarter = tab.excel_ref('B6').expand(DOWN)-remove_notes
+        period = quarter.shift(LEFT).is_not_blank()-remove_notes 
+#         sheet_name = tab.name
+#         savepreviewhtml(period, fname= tab.name + "PREVIEW.html")
+    
+        prevention_duty = tab.excel_ref('D3').expand(RIGHT)
+        tenancy_type = tab.excel_ref('D4').expand(RIGHT)
+        reasons_for_breach = tab.excel_ref('D5').expand(RIGHT)
+        reasons_for_rent_arrears = tab.excel_ref('D6').expand(RIGHT)
+        observations = tab.excel_ref('D7').expand(RIGHT).expand(DOWN)-remove_notes
+#         savepreviewhtml(observations, fname= tab.name + "PREVIEW.html")
+        dimensions = [
+            HDim(quarter,'quarter',DIRECTLY,LEFT),
+            HDim(period,'period',CLOSEST,ABOVE),
+            HDim(prevention_duty,'prevention_duty',DIRECTLY, ABOVE),
+            HDim(tenancy_type,'tenancy_type',DIRECTLY, ABOVE),
+            HDim(reasons_for_breach,'reasons_for_breach',DIRECTLY, ABOVE),
+            HDim(reasons_for_rent_arrears,'reasons_for_rent_arrears',DIRECTLY, ABOVE),
+            #HDimConst("sheet_name", sheet_name) #Might be handy to have for post processing when other tabs are running also 
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname= tab.name + "PREVIEW.html")
+        trace.with_preview(tidy_sheet)
+        trace.store("combined_dataframe", tidy_sheet.topandas())
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+#Sheet = "A1"
+df.drop(['initial_assessment', 'duty_owed', 'section_21'], axis=1, inplace=True)
+df
+
+#Number of households owed a relief duty by reason for loss, of last settled home England
+for tab in tabs:
+    columns=['Contents']
+    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
+    if tab.name in ['A2R']: #only transforming tab A1 for now
+        print(tab.name)
+        
+        remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
+        quarter = tab.excel_ref('B7').expand(DOWN)-remove_notes
+        period = quarter.shift(LEFT).is_not_blank()-remove_notes 
+#         sheet_name = tab.name
+#         savepreviewhtml(period, fname= tab.name + "PREVIEW.html")
+
+        relief_prevention_duty = tab.excel_ref('D3').expand(RIGHT)
+        relief_tenancy_type = tab.excel_ref('D4').expand(RIGHT)
+        relief_reasons_for_breach = tab.excel_ref('D5').expand(RIGHT)
+        relief_reasons_for_rent_arrears = tab.excel_ref('D6').expand(RIGHT)
+        observations = tab.excel_ref('D7').expand(RIGHT).expand(DOWN)-remove_notes
+#         savepreviewhtml(observations, fname= tab.name + "PREVIEW.html")
+        dimensions = [
+            HDim(quarter,'quarter',DIRECTLY,LEFT),
+            HDim(period,'period',CLOSEST,ABOVE),
+            HDim(relief_prevention_duty,'relief_prevention_duty',DIRECTLY, ABOVE),
+            HDim(relief_tenancy_type,'relief_tenancy_type',DIRECTLY, ABOVE),
+            HDim(relief_reasons_for_breach,'relief_reasons_for_breach',DIRECTLY, ABOVE),
+            HDim(relief_reasons_for_rent_arrears,'relief_reasons_for_rent_arrears',DIRECTLY, ABOVE),
+            #HDimConst("sheet_name", sheet_name) #Might be handy to have for post processing when other tabs are running also 
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname= tab.name + "PREVIEW.html")
+        trace.with_preview(tidy_sheet)
+        trace.store("combined_dataframe", tidy_sheet.topandas())
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+#Sheet = "A1"
+df.drop(['initial_assessment', 'duty_owed', 'section_21'], axis=1, inplace=True)
+#Sheet = "A2P"
+df.drop(['prevention_duty', 'tenancy_type', 'reasons_for_breach', 'reasons_for_rent_arrears'], axis=1, inplace=True)
+df
 
 

ce1a17886467c328b693d3bbe9e4024d92a66393
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-04-26 09:08:50 +0100

    transformation of Sheet - "A7"

--- datasets/MHCLG-Statutory-homelessness-in-England/main.py
+++ datasets/MHCLG-Statutory-homelessness-in-England/main.py
@@ -433,6 +433,58 @@ df.drop(['household_composition', 'gender'], axis=1, inplace=True)
 #Sheet = "A5R"
 df.drop(['relief_duty_household_composition', 'relief_duty_gender'], axis=1, inplace=True)
 df       
+# +
+# Number of households assessed as a result of a referral, including under the Duty to Refer England
+
+for tab in tabs:
+    columns=['Contents']
+    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
+    if tab.name in ['A7']: #only transforming tab A1 for now
+        print(tab.name)
+        
+        remove_notes = tab.filter(contains_string('Notes')).expand(DOWN).expand(RIGHT)
+        quarter = tab.excel_ref('B6').expand(DOWN)-remove_notes
+        period = quarter.shift(LEFT).is_not_blank()-remove_notes 
+#         sheet_name = tab.name
+#         savepreviewhtml(period, fname= tab.name + "PREVIEW.html")
+        total_referred_households = tab.excel_ref('D3').expand(RIGHT)
+        total_households_duty_refer2 = tab.excel_ref('D4').expand(RIGHT)
+        breakdown_total_households_duty_refer2 = tab.excel_ref('D5').expand(RIGHT)
+        observations = tab.excel_ref('D6').expand(DOWN).expand(RIGHT)-remove_notes
+#         savepreviewhtml(observations, fname= tab.name + "PREVIEW.html")
+        dimensions = [
+            HDim(quarter,'quarter',DIRECTLY,LEFT),
+            HDim(period,'period',CLOSEST,ABOVE),
+            HDim(total_referred_households,'total_referred_households',DIRECTLY, ABOVE),
+            HDim(total_households_duty_refer2,'total_households_duty_refer2',DIRECTLY, ABOVE),
+            HDim(breakdown_total_households_duty_refer2,'breakdown_total_households_duty_refer2',DIRECTLY, ABOVE),
+            #HDimConst("sheet_name", sheet_name) #Might be handy to have for post processing when other tabs are running also 
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname= tab.name + "PREVIEW.html")
+        trace.with_preview(tidy_sheet)
+        trace.store("combined_dataframe", tidy_sheet.topandas())
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+#Sheet = "A1"
+df.drop(['initial_assessment', 'duty_owed', 'section_21'], axis=1, inplace=True)
+#Sheet = "A2P"
+df.drop(['prevention_duty', 'tenancy_type', 'reasons_for_breach', 'reasons_for_rent_arrears'], axis=1, inplace=True)
+#Sheet = "A2R"
+df.drop(['relief_prevention_duty', 'relief_tenancy_type', 'relief_reasons_for_breach', 'relief_reasons_for_rent_arrears'], axis=1, inplace=True)
+#Sheet = "A3"
+df.drop(['total_households_with_supportneeds', 'households_with_one_supportneeds', 'households_with_two_supportneeds'], axis=1, inplace=True)
+#Sheet = "A4P"
+df.drop(['rented_sector', 'prs_srs', 'breakdown_of_prs_srs'], axis=1, inplace=True)
+#Sheet = "A4R"
+df.drop(['accomodation_during_application', 'breakdown_of_accomodation', 'accomodation_type'], axis=1, inplace=True)
+#Sheet = "A5P"
+df.drop(['household_composition', 'gender'], axis=1, inplace=True)
+#Sheet = "A5R"
+df.drop(['relief_duty_household_composition', 'relief_duty_gender'], axis=1, inplace=True)
+#Sheet = "A6"
+df.drop(['age_of_applicants'], axis=1, inplace=True)
+df
 # -
+
 
 
